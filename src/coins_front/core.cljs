a14c2f6b0a2d98d5dbf8d70e720881558188e95a
Vlad Aituganov <vlad.aituganov@gmail.com>
2016-05-15 17:21:52 +0300

    SVG test

--- src/coins_front/core.cljs
+++ src/coins_front/core.cljs
@@ -28,7 +28,11 @@
   [:div
    [counterHeader (:count @app-state)]
    [counterButton incCounter "Increase counter"]
-   [counterButton decCounter "Decrease counter"] ])
+   [counterButton decCounter "Decrease counter"]
+   [:div
+    [:svg {:width 100 :height 100} 
+     [:rect {:x 50 :y 50 :width 50 :height 50 :fill "#bbb"}]
+     [:rect {:x 0 :y 0 :width 50 :height 50 :fill "#bbb"}]]]])
 
 (r/render [app]
           (js/document.getElementById "app"))
