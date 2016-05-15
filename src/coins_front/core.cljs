9f9b4698c53d7522b3c42b6c210011d3d6bba432
Vlad Aituganov <vlad.aituganov@gmail.com>
2016-05-15 16:49:18 +0300

    Counter example finished

--- src/coins_front/core.cljs
+++ src/coins_front/core.cljs
@@ -1,13 +1,37 @@
 (ns coins-front.core
-  (:require ))
+  (:require [reagent.core :as r])) 
 
 (enable-console-print!)
 
-(js/console.clear)
 
-;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (r/atom {:count 0}))
+
+
+
+(defn counterHeader [counter]
+  [:h1 (str "Counter = " counter)])
+
+(defn counterButton [onClick caption]
+  [:button {:on-click onClick} caption])
+
+
+(defn mapCounter [ func ]
+  (let [curr (:count @app-state)]
+    (reset! app-state {:count (func curr)})))
+
+(defn incCounter [] (mapCounter inc))
+(defn decCounter [] (mapCounter dec))
+
+
+(defn app []
+  [:div
+   [counterHeader (:count @app-state)]
+   [counterButton incCounter "Increase counter"]
+   [counterButton decCounter "Decrease counter"] ])
+
+(r/render [app]
+          (js/document.getElementById "app"))
 
 
 (defn on-js-reload []
