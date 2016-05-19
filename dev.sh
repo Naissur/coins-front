d7a3a03d93ffd3ab6a7207418a7902fedc8085b2
Vlad Aituganov <vlad.aituganov@gmail.com>
2016-05-19 12:13:18 +0000

    Added tmux dev.sh

---
+++ dev.sh
@@ -0,0 +1,29 @@
+
+tmux new-session -d -s coins-front
+
+## Whiteboard session
+
+# Org-mode window
+tmux select-window -t coins-front:0
+tmux rename-window 'org'
+
+# Build window
+tmux new-window -n build -t coins-front
+tmux send-keys 'clear' 'Enter'
+tmux send-keys 'lein figwheel'
+tmux send-keys 'Enter'
+
+# Source window
+tmux new-window -n src -t coins-front
+tmux send-keys 'cd src/coins_front' 'Enter'
+tmux send-keys 'clear' 'Enter'
+tmux send-keys 'vim'
+tmux send-keys 'Enter'
+
+# Directory window
+tmux new-window -n dir -t coins-front
+tmux send-keys 'clear' 'Enter'
+tmux send-keys 'Enter'
+
+
+tmux attach-session -t coins-front
